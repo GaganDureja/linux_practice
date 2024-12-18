5fa8e97679b1106016f4286c105e4682235a4b8e
GaganDureja <gagandureja675@gmail.com>
2024-12-18 10:23:25 +0530

    updated script to require name when script is started

--- 16 dec/script.sh
+++ 16 dec/script.sh
@@ -3,12 +3,6 @@ if [ -z "$NAME" ]; then
   echo "Name cannot be empty. Please run the script again and provide a valid name."
   exit 1
 fi
-echo "Available data: fname, lname, email, mobile, country"
-read -p "Please enter data you want to retrieve: " COLUMN
-if [ -z "$COLUMN" ]; then
-  echo "Column name cannot be empty. Please run the script again and provide a valid column name."
-  exit 1
-fi
 FOUND=false
 while IFS='|' read -r fname lname email mobile country; do
   if [ "$fname" == "fname" ]; then
@@ -16,28 +10,13 @@ while IFS='|' read -r fname lname email mobile country; do
   fi
   if [[ "${fname,,}" == "${NAME,,}" || "${lname,,}" == "${NAME,,}" ]]; then
     FOUND=true
-    case $COLUMN in
-      fname)
         echo "First Name: $fname"
-        ;;
-      lname)
         echo "Last Name: $lname"
-        ;;
-      email)
         echo "Email: $email"
-        ;;
-      mobile)
         echo "Mobile: $mobile"
-        ;;
-      country)
         echo "Country: $country"
-        ;;
-      *)
-        echo "Invalid column name: $COLUMN. Please run the script again and use a valid column name."
         exit 1
-        ;;
-    esac
-    break
+    # break
   fi
 done < file.csv
 if [ "$FOUND" = false ]; then
